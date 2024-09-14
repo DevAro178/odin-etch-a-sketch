203cfd713104dd3f8f846bb43abef2d1e4799ef4
Muhammad Ammad <me.ammad1786@gmail.com>
2024-09-14 18:37:57 +0500

    Added button to change the grid size

--- script.js
+++ script.js
@@ -1,10 +1,32 @@
 let gridCount=16
 let sketchBoardWidth=650
 let container=document.querySelector('div.container')
-container.style.maxWidth=sketchBoardWidth+'px'
+let gridSizeBtn=document.querySelector('div.title div.btn')
 let itemDimensions=sketchBoardWidth/gridCount
 let clicked=false
 
+gridSizeBtn.addEventListener('click',e=>{
+    let newGridSize=parseInt(prompt("Enter grid size between 1-100"))
+    if(newGridSize>0 && newGridSize<=100){
+        gridCount=newGridSize
+        itemDimensions=sketchBoardWidth/gridCount
+        createSketchBoard()
+    }
+    else
+        alert("You entered invalid value")
+})
+
+container.style.maxWidth=sketchBoardWidth+'px'
+container.addEventListener('mousedown',e=>{
+    clicked=true
+    sketch(e.target)
+})
+container.addEventListener('mouseup',e=>{
+    clicked=false
+})
+container.addEventListener('drag',e=>{
+    clicked=false
+})
 
 let createElementWithClassName=(type,...className)=>{
     let element=document.createElement(type)
@@ -23,33 +45,26 @@ let createRow=()=>{
     }
 }
 
-let createSketchBoard=()=>{
-    for(let i=0;i<gridCount;i++){
-        createRow()
-    }
-}
-
 let sketch=(element)=>{
     element.classList.add('active')
 }
 
+let createSketchBoard=()=>{
+    container.innerHTML=''
+
+    for(let i=0;i<gridCount;i++){
+        createRow()
+    }
+
+    let items=document.querySelectorAll("div.item")
+    items.forEach(item=>{
+        item.addEventListener('mouseenter',e=>{
+            if(clicked)
+                sketch(e.target)
+        })
+    })
+}
+
+
 createSketchBoard()
 
-let items=document.querySelectorAll("div.item")
-
-container.addEventListener('mousedown',e=>{
-    clicked=true
-    sketch(e.target)
-})
-container.addEventListener('mouseup',e=>{
-    clicked=false
-})
-container.addEventListener('drag',e=>{
-    clicked=false
-})
-items.forEach(item=>{
-    item.addEventListener('mouseenter',e=>{
-        if(clicked)
-            sketch(e.target)
-    })
-})
